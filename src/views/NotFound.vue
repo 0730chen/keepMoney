fb290a1c53b4bb3471cc668869e223cfb739f51f
0730chen <1252331215@qq.com>
2020-02-15 18:06:49 +0800

    创建添加NotFound

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        NotFound当前页面不存在
+        <router-link to="/money">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+    import {Component, Prop, Vue} from 'vue-property-decorator';
+
+    @Component
+    export default class NotFound extends Vue {
+        name: "NotFound" | undefined
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
