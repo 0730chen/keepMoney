54d8b0e98d272af3f41aef07dbe11e1a0fa01e73
0730chen <1252331215@qq.com>
2020-02-18 16:17:41 +0800

    重新封装data

--- src/models/moneyModel.ts
+++ src/models/moneyModel.ts
@@ -1,13 +1,15 @@
 const localStrogeKeyName = 'moneyModel'
 const moneyModel = {
+    data:[] as RecordItem[],
     clone(data: RecordItem[]|RecordItem){
         return JSON.parse(JSON.stringify(data))
     },
     fetch() {
-        return JSON.parse(window.localStorage.getItem(localStrogeKeyName) || '[]') as RecordItem[];
+        const result = JSON.parse(window.localStorage.getItem(localStrogeKeyName) || '[]') as RecordItem[];
+        return result
     },
     save(data: RecordItem[]) {
-        window.localStorage.setItem(localStrogeKeyName, JSON.stringify(data))
+        window.localStorage.setItem(localStrogeKeyName, JSON.stringify(this.data))
     }
 }
 export default moneyModel
